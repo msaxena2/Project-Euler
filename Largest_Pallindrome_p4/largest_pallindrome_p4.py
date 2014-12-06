11f656ce1866ca30d43b4ef24427001fc90a9a30
Manasvi Saxena <msaxena2@illinois.edu>
2014-12-06 12:04:24 -0600

    added p4 sol

--- Largest_Pallindrome_p4/largest_pallindrome_p4.py
+++ Largest_Pallindrome_p4/largest_pallindrome_p4.py
@@ -1,1 +1,30 @@
 __author__ = 'manasvi'
+
+
+def is_palindrome(num):
+    rev = 0
+    temp_num = num
+    while(temp_num > 0):
+        rev = (rev * 10) + ((temp_num) % 10)
+        temp_num = temp_num / 10
+    return rev == num
+
+
+
+def find_palindrome():
+    max_palindrome = 0
+    for i in xrange(1000, 0, -1):
+        for j in xrange(1000, 0, -1):
+            if is_palindrome(i * j) and (i * j) > max_palindrome:
+                max_palindrome = (i * j)
+    return max_palindrome
+
+def main():
+    p = find_palindrome()
+    if(p > 0):
+        print p
+    else:
+        print "none"
+
+if __name__ == '__main__':
+    main()
