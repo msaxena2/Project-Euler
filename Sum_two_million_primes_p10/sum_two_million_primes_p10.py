97ca4c4a0fbb83a906f1f0d73ee1eee25f438030
Manasvi <msaxena2@illinois.edu>
2014-12-27 20:38:05 +0530

    added description

--- Sum_two_million_primes_p10/sum_two_million_primes_p10.py
+++ Sum_two_million_primes_p10/sum_two_million_primes_p10.py
@@ -1,5 +1,9 @@
 __author__ = 'manasvi'
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
+Find the sum of all the primes below two million.
+"""
 
 def find_sum(limit):
     arr = [1] * limit
@@ -7,12 +11,12 @@ def find_sum(limit):
     sum = 0
     for i in xrange(2, limit+1):
         if arr[i-1] == 1:
-            print i
             #number is prime
             sum += i
             for j in xrange(i, limit+1, i):
                 arr[j-1] = -1
     return sum
 
+#change n here
 print find_sum(2000000)
 
