b3b4a478e9846353f422de455870163d28356bda
Manasvi <msaxena2@illinois.edu>
2015-01-14 20:05:18 +0530

    11

--- Largest_Product_In_Grid_p11/largest_product_in_grid_p11.py
+++ Largest_Product_In_Grid_p11/largest_product_in_grid_p11.py
@@ -8,6 +8,22 @@ def parse_input(filename):
 
 
 def find_max_product(matrix):
-    
+   max_product = -1
+   for i in xrange(0, len(matrix)):
+       local_max = -1
+       for j in xrange(0, len(matrix[0])):
+           if (j + 4) < len(matrix[0]):
+               sub_max = matrix[i][j] * matrix[i][j+1] * matrix[i][j+2] * matrix[i][j+3]
+               if sub_max > max_product:
+                   max_product = sub_max
+           if (i + 4) < len(matrix):
+               sub_max = matrix[i][j] * matrix[i+1][j] * matrix[i+2][j] * matrix[i+3][j]
+               if sub_max > max_product:
+                   max_product = sub_max
+           if (i + 4) < len(matrix) and (j + 4) < len(matrix[0]):
+               sub_max = matrix[i][j] * matrix[i+1][j+1] * matrix[i+2][j+2] * matrix[i+3][j+3]
+               if sub_max > max_product:
+                   max_product = sub_max
+   print(max_product)
 
-print parse_input("input.txt")[2][3]
+find_max_product(parse_input("input.txt"))
