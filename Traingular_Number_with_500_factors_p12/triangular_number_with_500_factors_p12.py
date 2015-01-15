e51d144be9235bfb0e8a27862773931d8fa82b5b
Manasvi <msaxena2@illinois.edu>
2015-01-15 14:14:20 +0530

    working on more optimized sol

--- Traingular_Number_with_500_factors_p12/triangular_number_with_500_factors_p12.py
+++ Traingular_Number_with_500_factors_p12/triangular_number_with_500_factors_p12.py
@@ -1,16 +1,31 @@
 __author__ = 'manasvi'
 from math import sqrt
 
-def fact_count(num):
-    count = 0
-    for i in xrange(1, int(sqrt(num)) + 1):
+prime_list = [2]
+
+def is_prime(num):
+    for i in range(2, int(sqrt(num)) + 1):
         if num % i == 0:
-            count += 1
-        return count
+            return False
+    return True
+
+def fact_count(num):
+    #populate prime list
+    for i in range(prime_list[-1], int(sqrt(num) + 1)):
+        if is_prime(i):
+            prime_list.append(i)
+
+    prime_dict = {}
+
+    while i < len(prime_list):
+
+
+
 start = 100
 while(True):
-    t_num = start*(start+1) / 2
-    if fact_count(t_num) > 500:
+    t_num = (start*(start+1)) / 2
+    fact = fact_count(t_num)
+    if fact >= 500:
         print t_num
         break
     start += 1
