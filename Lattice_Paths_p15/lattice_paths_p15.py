028f0fb5b02588b367bc8d61e43def7d710e225d
Manasvi <msaxena2@illinois.edu>
2015-01-16 13:59:48 +0530

    added comment

--- Lattice_Paths_p15/lattice_paths_p15.py
+++ Lattice_Paths_p15/lattice_paths_p15.py
@@ -1,6 +1,7 @@
 __author__ = 'manasvi'
 from math import factorial
 def lattice_paths(n):
+    #Optimaztion possible here, by reducing the below formula
     return factorial(2 * n) / (factorial(n) * factorial(n))
 
 #change n here
