66a1acc2622b0d625f5390299b9d567209db2747
Manasvi <msaxena2@illinois.edu>
2015-03-18 01:56:30 -0500

    working on sol for p19

--- Counting_Sundays_p19/counting_sundays_p19.py
+++ Counting_Sundays_p19/counting_sundays_p19.py
@@ -1,1 +1,33 @@
 __author__ = 'manasvi'
+
+day_count = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+def count_sunday_year(day, leap_status):
+
+    sunday_count = 0
+    for i in range(0, 12):
+        if i != 2:
+            day = (day + day_count[i]) % 7
+
+        else:
+            if leap_status == "leap":
+                day = (day + 29) % 7
+            else:
+                day = (day + day_count[i]) % 7
+
+        if (day == 1):
+            sunday_count += 1
+
+    return (sunday_count, day)
+
+init_day = 3
+total_count = 0
+for i in range(1901, 1902):
+    if (i % 100) != 0 and (i % 4) == 0:
+        count, init_day = count_sunday_year(init_day, "leap")
+    else:
+        count, init_day = count, init_day = count_sunday_year(init_day, "unleap")
+    total_count += count
+
+print(total_count)
